4709eb6510a6f9e2f560940b56eb6c300a52665f
Ser_Yo_Ja <seroja.92@mail.ru>
2022-01-06 01:23:59 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/frags/recycler/NoteHolder.java
@@ -0,0 +1,37 @@
+package ru.gb.frags.recycler;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import ru.gb.frags.R;
+import ru.gb.frags.data.Note;
+
+
+public class NoteHolder extends RecyclerView.ViewHolder {
+
+    private TextView title;
+    private TextView description;
+    private Note note;
+
+
+    public NoteHolder(@NonNull View itemView, NotesAdapter.OnNoteClickListener listener) {
+        super(itemView);
+        title = itemView.findViewById(R.id.note_title);
+        description = itemView.findViewById(R.id.note_description);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onNoteClick(note);
+            }
+        });
+    }
+
+    void bind(Note note){
+        this.note = note;
+        title.setText(note.getTitle());
+        description.setText(note.getDescription());
+    }
+}
